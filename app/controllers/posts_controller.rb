f379e4968f538830e837d47c9307b3e7494a07a2
Brandon Weaver <a7xncob@gmail.com>
2020-01-22 19:02:20 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,20 @@
 class PostsController < ApplicationController
+  
   def index
     @posts = Post.all
   end
-
+  
   def show
     @post = Post.find(params[:id])
   end
-
+  
   def new
     @post = Post.new
   end
-
+  
   # add create method here
+  def create
+    
+  end
+  
 end
